93962a7f75bca35f494dd5686e622d4492880230
Michael Mogenson <michael.mogenson@gmail.com>
2019-03-02 13:45:06 -0500

    Add comments and switch up program flow a bit

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-var contextID = 0; // 0 sends keys to non-input field
-var savedEscKey = null;
-var ctrlKeySent = false;
+/*****************************************************************************
+ * This Chrome OS extension will map the Escape key to a Control key if it's
+ * used as a modifier with another key. So press and release Escape by itself
+ * to emit an Escape key event. Hold Escape and press another key to emit a
+ * Control + other key event. In the second case, the Escape down and up events
+ * will not be sent.
+ *****************************************************************************/
+
+var contextID = 0;      // zero is supposed to send keys to non-input field
+var savedEscKey = null; // save the escape key down event to send later
+
+/* update the context when it changes */
 
 chrome.input.ime.onFocus.addListener(function(context) {
     contextID = context.contextID;
@@ -14,51 +23,49 @@ chrome.input.ime.onInputContextUpdate.addListener(function(context) {
     contextID = context.contextID;
 });
 
+/* handle keyboard input, return true to say we've modified this input */
+
 chrome.input.ime.onKeyEvent.addListener(function(engineID, keyData) {
-    var handled = false; // return true to say we've modified this input
+    //console.log('onKeyEvent', keyData); // DEBUGGING
 
     /* don't re-modify our own output */
-    if (keyData.extensionId == chrome.runtime.id) return handled;
-
-    /* DEBUGGING */
-    //console.log('onKeyEvent', keyData);
+    if (keyData.extensionId == chrome.runtime.id) return false;
 
     if (keyData.type == "keydown") {
-        if (keyData.code == "Escape") {
-            /* don't sent escape key
-             * wait for another key to see if it needs to be turned
-             * into a control key */
+
+        /* don't sent escape key, wait to see if it needs to be turned into a control key */
+        if (keyData.code == "Escape" && !keyData.ctrlKey) {
             savedEscKey = keyData;
-            handled = true;
-        } else if (savedEscKey) {
-            /* if escape is held down, pass along any key inputs with the
-             * control key modifier set */
-            keyData.ctrlKey = true;
-            ctrlKeySent = true;
-            chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
-            handled = true;
+            return true;
         }
-    } else if (keyData.type == "keyup") {
+
+        /* if escape is held down, pass along any key inputs with the control key modifier set */
+        if (savedEscKey) {
+            savedEscKey.ctrlKey = true; // used to store ctrl key sent status
+            keyData.ctrlKey = true;     // Chrome OS doesn't need a separate ctrl key event
+            chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
+            return true;
+        }
+
+    } else if (keyData.type == "keyup" && savedEscKey) {
+
+        /* if we have an escape key down event that we've stored and possibly modified */
         if (keyData.code == "Escape") {
-            if (ctrlKeySent) {
-                /* escape was used as a control modifier
-                 * swallow the escape keyup and keydown */
-                savedEscKey = null;
-                ctrlKeySent = false;
-                handled = true;
-            } else {
-                /* no other keys were pressed, send escape keydown and keyup */
+            if (!savedEscKey.ctrlKey) // no other keys were pressed, send escape keydown and keyup
                 chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [savedEscKey, keyData]});
-                savedEscKey = null;
-                handled = true;
-            }
-        } else if (ctrlKeySent) {
-            /* pass along key inputs with the control key modifier set */
+
+            /* otherwise escape was used as a ctrl modifier, discard the keyup and keydown events */
+            savedEscKey = null; // clear saved keydown event
+            return true;
+        }
+
+        /* pass along key inputs with the control key modifier set */
+        if (savedEscKey.ctrlKey) {
             keyData.ctrlKey = true;
             chrome.input.ime.sendKeyEvents({"contextID": contextID, "keyData": [keyData]});
-            handled = true;
+            return true;
         }
     }
 
-    return handled;
+    return false;
 });
